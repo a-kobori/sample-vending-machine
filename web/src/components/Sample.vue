6a76a064c9437bf2fe1d21c85ed3708b5806700a
Kobori Akira <a_kobori@codmon.co.jp>
2023-03-09 23:55:17 +0900

    フロントエンドをシンプルにする

---
+++ web/src/components/Sample.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      name: 'Unknown'
+    }
+  },
+  methods: {
+    async getSample(id: number) {
+      const result = await axios.get('http://localhost:3202/api/sample/' + id)
+      this.name = result.data[0].name
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>Hello, {{ name }}</h1>
+  <p>これはSample.vueで描画されています</p>
+  <p>
+    このボタンを押すと、sampleテーブルの指定されたidのレコードのnameを取得します<br />
+    <ul>
+      <li><button v-on:click="getSample(1)">id:1</button></li>
+      <li><button v-on:click="getSample(2)">id:2</button></li>
+    </ul>
+  </p>
+</template>
